760cfd9139b505a6d021334b9d8ae28b6ebc6089
cdhigh <cdhigh@users.noreply.github.com>
2024-06-15 06:52:18 -0300

    Update build_hunspell.py

--- build_hunspell.py
+++ build_hunspell.py
@@ -51,7 +51,7 @@ def build_hunspell_package(directory, force_build=False):
         hunspell_library_name = 'libhunspell-1.7.2.dylib'
         export_lib_name = 'hunspell-1.7.2'
         build_lib_path = os.path.join(BASE_DIR, 'external', 'build', 'lib', 'libhunspell-1.7.0.dylib')
-        configure_flags = ['LDFLAGS=-static-libgcc', 'CXXFLAGS=-static-libgcc']
+        configure_flags = []
     hunspell_so_path = os.path.join(hunspell_so_dir, hunspell_library_name)
 
     olddir = os.getcwd()
